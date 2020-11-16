cfb4c3a7c3ccf235b9b00ece6774f4256c6da0d3
cpetryka <cezaryp10@gmail.com>
2020-11-16 21:00:57 +0100

    add 'find_by_parameters' to 'SeatRepository'

--- repository/seat_repository.hpp
+++ repository/seat_repository.hpp
@@ -15,6 +15,7 @@ public:
     void update(const int id, const Seat& seat);
     void remove(const int id);
     int find_pos(const Seat& seat);
+    std::optional<std::unique_ptr<Seat>> find_by_parameters(const int cinema_room_id, const int row, const int place);
 };
 
 
--- repository/sources/seat_repository.cpp
+++ repository/sources/seat_repository.cpp
@@ -75,4 +75,25 @@ int SeatRepository::find_pos(const Seat &seat) {
     return -1;
 }
 
+std::optional<std::unique_ptr<Seat>> SeatRepository::find_by_parameters(const int cinema_room_id, const int row, const int place) {
+    const std::string sql = "select id from seats where cinema_room_id = ? and row = ? and place = ?";
+    const auto connection = DbConnection::get_instance()->get_connection();
+    sqlite3_stmt* stmt;
+    sqlite3_prepare_v2(connection, sql.c_str(), -1, &stmt, nullptr);
+    sqlite3_bind_int(stmt, 1, cinema_room_id);
+    sqlite3_bind_int(stmt, 2, row);
+    sqlite3_bind_int(stmt, 3, place);
 
+    auto result = 0;
+    while ((result = sqlite3_step(stmt)) == SQLITE_ROW) {
+        return std::make_optional(std::make_unique<Seat>(Seat{
+                sqlite3_column_int(stmt, 0),
+                cinema_room_id,
+                row,
+                place
+        }));
+    }
+
+    sqlite3_finalize(stmt);
+    return std::nullopt;
+}
